e4fd27ce2e9af81599660f915837e308c3f12f34
hyewon <namhiewon@gmail.com>
2024-08-27 13:31:36 +0900

    Profile 컴포넌트 만드는 중 ch01-14-2 13:29

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,3 +1,20 @@
-const Profile = () => {};
+import React from 'react';
+import {Image, StyleProp, TouchableOpacity, View} from 'react-native';
+
+//Props 정의
+interface ProfileProps {
+  size?: number;
+  style?: StyleProp;
+  onPress?: () => void;
+  imageUrl?: string;
+}
+
+const Profile = ({size, style, onPress, imageUrl}: ProfileProps) => {
+  return (
+    <TouchableOpacity disabled={onPress == null} onPress={onPress}>
+      <View>{imageUrl && <Image />}</View>
+    </TouchableOpacity>
+  );
+};
 
 export default Profile;
